2107a6a270bc057e5d0fa2e935d0036e2392bdbd
Babazile <mpongozababazile@gmail.com>
2018-04-26 15:42:48 +0200

    added a dept-exercise file

--- dept.js
+++ dept.js
@@ -1,16 +1,38 @@
-console.log("hello i'm Babazile")
+function grandTotal(departmentsTotal){
+  var total = 0;
+  for(var i=0;i< departmentsTotal.length;i++){
+    total += (departmentsTotal[i].total);
+     console.log(total);
+  }
+  return total;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var ToletriesDept = {name: "toletries", total: 5007};
 var GroceriesDept  = {name: "Groceries", total: 11089};
 var PharmacyDept = {name: "Pharmacy", total: 7321};
 var BakeryDept = {name: "Bakery", total: 10073};
 var ButcheryDept = {name: "Butchery", total: 9053};
 
-//
-// console.log(GroceriesDept.name)
-// console.log(ToletriesDept.name)
-// console.log(PharmacyDept.name)
-// console.log(BakeryDept.name)
-// console.log(ButcheryDept.name);
+console.log(GroceriesDept.name)
+ console.log(ToletriesDept.name)
+ console.log(PharmacyDept.name)
+ console.log(BakeryDept.name)
+ console.log(ButcheryDept.name);
 
 
 
@@ -21,14 +43,15 @@ depts.push(GroceriesDept);
 depts.push(BakeryDept);
 depts.push(ButcheryDept);
 
-function grandTotal(departmentsTotal){
+/*unction grandTotal(departmentsTotal){
   var total = 0;
   for(var i=0;i< depts.length;i++){
     total += (depts[i].total);
-    // console.log(depts[i].name);
-    // console.log(depts[i].total);
-    // var grandTotal = grandTotal.split(',');
+    console.log(depts[i].name);
+     console.log(depts[i].total);
+  //  var grandTotal = grandTotal.split(',');
   }
   console.log(total);
 }
 grandTotal(depts)
+*/
--- grandTotal.js
+++
@@ -1,8 +0,0 @@
-function grandTotal(departmentsTotal){
-  var total = 0;
-  for(var i=0;i< departmentsTotal.length;i++){
-    total += (departmentsTotal[i].total);
-     console.log(total);
-  }
-  return total;
-}
